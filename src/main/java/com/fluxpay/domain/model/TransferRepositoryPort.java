1e107b9da6ac02cc11802ac07b0cb5a950a3a520
Zairex-Code <dylanyesidflorez@gmail.com>
2026-07-18 23:01:21 -0500

    Feature/ TransferRepositoryPort interface created

---
+++ src/main/java/com/fluxpay/domain/model/TransferRepositoryPort.java
@@ -0,0 +1,24 @@
+package com.fluxpay.domain.model;
+
+import reactor.core.publisher.Mono;
+
+public interface TransferRepositoryPort {
+    /**
+     * Saves a transfer into underlying persistence mechanism
+     * 
+     * @param transfer The transfer domain entity
+     * @return A Mono emitting the saved transfer.
+     */
+    Mono<Transfer> save(Transfer transfer);
+
+    /**
+     * Finds a transfer by its unique identifier
+     * 
+     * @param id The unique identifier of the transfer
+     * @return A Mono emitting the found transfer, or empty if not found.
+     */
+
+    Mono<Transfer> findById(String id);
+
+
+}
