846a820998787642130d3b13b051896f9993db2e
Zairex-Code <dylanyesidflorez@gmail.com>
2026-07-24 16:40:05 -0500

    feature/ Validate transfer implemented into MakeTransferInteractor

--- src/main/java/com/fluxpay/application/usecase/MakeTransferInteractor.java
+++ src/main/java/com/fluxpay/application/usecase/MakeTransferInteractor.java
@@ -1,19 +1,24 @@
 package com.fluxpay.application.usecase;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 import com.fluxpay.application.port.in.MakeTransferUseCase;
 import com.fluxpay.application.port.out.TransferRepositoryPort;
+import com.fluxpay.domain.exception.InvalidTransferException;
 import com.fluxpay.domain.model.Transfer;
 import com.fluxpay.domain.model.TransferStatus;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
 /**
  * Domain service that implements the specific use case for making a transfer.
  * Notice the absence of Spring annotations. This is pure Java.
  */
+
+@Slf4j
 @RequiredArgsConstructor
 public class MakeTransferInteractor implements MakeTransferUseCase {
 
@@ -21,17 +26,30 @@ public class MakeTransferInteractor implements MakeTransferUseCase {
 
     @Override
     public Mono<Transfer> execute(Transfer transfer) {
-        // Here we will eventually add business validations
-        // For example: Checking sufficient funds, verifying account status, etc.
+
+        return Mono.justOrEmpty(transfer)
+            .switchIfEmpty(Mono.error(new InvalidTransferException("Transfer request payload cannot be null")))
+            .flatMap(this::validateTransfer)
+            .flatMap(transferRepositoryPort::save)
+            .doOnSuccess(savedTransfer -> 
+                            log.info("Transfer successfully executed with ID: {}", savedTransfer.getId()))
+            .onErrorResume(InvalidTransferException.class, ex -> {
+                log.error("Domain business rule violation: {} ", ex.getMessage());
+                return Mono.error(ex);
+            });
         
-        // For now, we simply set an initial status and delegate to the outbound port.
-        // We use Reactive Streams (Mono) to ensure non-blocking execution.
+    }
 
-        transfer.setStatus(TransferStatus.PENDING);
-        transfer.setCreatedAt(LocalDateTime.now());
+    private Mono<Transfer>  validateTransfer(Transfer transfer){
+        if(transfer.getAmount()== null || transfer.getAmount().compareTo(BigDecimal.ZERO) <= 0 ){
+            return Mono.error(new InvalidTransferException("Transfer amount must be strictly greater than zero"));
+        }
 
-        return Mono.just(transfer)
-                // .flatMap(t -> validateAccounts(t)) -> SAGA step in the future
-                .flatMap(transferRepositoryPort::save);
+        if(transfer.getOriginAccount() != null && transfer.getOriginAccount().equals(transfer.getDestinationAccount())){
+            return Mono.error(new InvalidTransferException("Source and target account cant be the same"));
+        }
+
+        return Mono.just(transfer);
+
     }
 }
