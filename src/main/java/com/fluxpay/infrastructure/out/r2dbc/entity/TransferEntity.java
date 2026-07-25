b0b65fa3059f931df2943384f5bd9df76a86a61c
Zairex-Code <dylanyesidflorez@gmail.com>
2026-07-24 23:29:39 -0500

    chore(entity): update transferEntity

--- src/main/java/com/fluxpay/infrastructure/out/r2dbc/entity/TransferEntity.java
+++ src/main/java/com/fluxpay/infrastructure/out/r2dbc/entity/TransferEntity.java
@@ -1,46 +1,42 @@
 package com.fluxpay.infrastructure.out.r2dbc.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.domain.Persistable;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.math.BigDecimal;
+import java.time.Instant;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
-@ToString
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString(exclude = {"amount"})
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Table("transfers") 
-public class TransferEntity implements Persistable<String> {
+public class TransferEntity {
 
     @Id
+    @EqualsAndHashCode.Include
     private String id;
+
+    @Column("origin_account")
     private String originAccount;
+
+    @Column("desination_account")
     private String destinationAccount;
+
     private BigDecimal amount;
+
     private String status;
-    private LocalDateTime createdAt;
 
-    // Este campo no se guardará en la base de datos gracias a @Transient.
-    // Le indicamos a Spring que, por defecto, todo objeto construido en memoria es nuevo.
-    @Transient
-    @Builder.Default
-    private boolean isNewRecord = true;
+    private String description;
 
-    // Método de la interfaz Persistable. Spring lo llamará para decidir entre INSERT o UPDATE.
-    @Override
-    @Transient
-    public boolean isNew() {
-        return this.isNewRecord;
-    }
+    private Instant createdAt;
+
 }
