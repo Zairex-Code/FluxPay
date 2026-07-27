a923abffcc0488e849f969deff9b6bcf7cced5cb
Zairex-Code <dylanyesidflorez@gmail.com>
2026-07-26 23:05:37 -0500

    refactor(usecase): observability implemented into Get All Transfers Interactor

--- src/main/java/com/fluxpay/application/usecase/GetAllTransferInteractor.java
+++ src/main/java/com/fluxpay/application/usecase/GetAllTransferInteractor.java
@@ -5,14 +5,18 @@ import com.fluxpay.application.port.out.TransferRepositoryPort;
 import com.fluxpay.domain.model.Transfer;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 
+@Slf4j
 @RequiredArgsConstructor
 public class GetAllTransferInteractor implements GetAllTransfersUseCase{
     private final TransferRepositoryPort transferRepositoryPort;
 
     @Override
     public Flux<Transfer> execute() {
+        log.debug("Fetching all recorded transfers");
+
         return transferRepositoryPort.findAll();
     }
 
