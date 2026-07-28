cd1af8bcffd4f12a57c9c63c58a61cb8cfb7e1f8
Zairex-Code <dylanyesidflorez@gmail.com>
2026-07-28 02:03:46 -0500

    refacture(usecases): MakeTransferInteractor updated

--- src/main/java/com/fluxpay/application/usecase/MakeTransferInteractor.java
+++ src/main/java/com/fluxpay/application/usecase/MakeTransferInteractor.java
@@ -4,13 +4,17 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 import com.fluxpay.application.port.in.MakeTransferUseCase;
+import com.fluxpay.application.port.out.ExternalBankPort;
+import com.fluxpay.application.port.out.TransferEventPort;
 import com.fluxpay.application.port.out.TransferRepositoryPort;
+import com.fluxpay.domain.event.TransferCreatedEvent;
 import com.fluxpay.domain.exception.InvalidTransferException;
 import com.fluxpay.domain.model.Transfer;
 import com.fluxpay.domain.model.TransferStatus;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 /*
@@ -21,15 +25,18 @@ import reactor.core.publisher.Mono;
 * */
 
 @Slf4j
+@Service
 @RequiredArgsConstructor
 public class MakeTransferInteractor implements MakeTransferUseCase {
 
     private final TransferRepositoryPort transferRepositoryPort;
+    private final ExternalBankPort externalBankPort;
+    private final TransferEventPort transferEventPort;
 
     /**
      * Execute a new Money Transfer transaction reactively
-     * Validates domain invariants, marks the transfer as completed, persists the entity via
-     * the repository port, and records operational logs
+     * Validates domain invariants, performs external bank checks, marks the transfer as completed,
+     * persists the entity via the repository port, emits a domain event to kafka , and records operational logs
      *
      * @Param transfer The domain model containing transfer request payload
      * @return A mono emitting the saved and completed Transfer domain model
@@ -45,11 +52,27 @@ public class MakeTransferInteractor implements MakeTransferUseCase {
         return Mono.justOrEmpty(transfer)
             .switchIfEmpty(Mono.error(new InvalidTransferException("Transfer request payload cannot be null"))) //If the input object is null, emit a Mono.error
             .flatMap(this::validateTransfer)
-            .map(Transfer::markAsCompleted)
-            .flatMap(transferRepositoryPort::save)
-            .doOnSuccess(savedTransfer -> log.info("Transfer successfully executed with ID: {}", savedTransfer.getId()))
-            .doOnError(InvalidTransferException.class, ex -> log.error("Domain business rule violation: {}", ex.getMessage()));
-        
+                .flatMap(validTransfer -> externalBankPort.verifyExternalAccount(
+                        validTransfer.getOriginAccount(),
+                        validTransfer.getAmount()
+                ).thenReturn(validTransfer))
+                .map(Transfer::markAsCompleted)
+                .flatMap(transferRepositoryPort::save)
+                .flatMap(savedTransfer -> {
+                    TransferCreatedEvent event = new TransferCreatedEvent(
+                            savedTransfer.getId(),
+                            savedTransfer.getOriginAccount(),
+                            savedTransfer.getDestinationAccount(),
+                            savedTransfer.getAmount(),
+                            savedTransfer.getStatus(),
+                            savedTransfer.getDescription(),
+                            savedTransfer.getCreatedAt()
+                    );
+                    return transferEventPort.publishTransferCreated(event)
+                            .thenReturn(savedTransfer);
+                })
+                .doOnSuccess(savedTransfer -> log.info("Transfer successfully execute with: {}", savedTransfer.getId()))
+                .doOnError(InvalidTransferException.class, ex -> log.error("Domain bussines rule violation: {}", ex.getMessage()));
     }
 
 
--- src/main/java/com/fluxpay/infrastructure/out/kafka/TransferKafkaAdapter.java
+++ src/main/java/com/fluxpay/infrastructure/out/kafka/TransferKafkaAdapter.java
@@ -37,5 +37,5 @@ public class TransferKafkaAdapter implements TransferEventPort {
 
     }
 
-    private
+
 }
